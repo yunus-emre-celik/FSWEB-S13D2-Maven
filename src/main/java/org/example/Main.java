f111c6f9f2df298b11b75934a7b14205845e48ab
yunus-emre-celik <yunus.celik.dev@gmail.com>
2024-11-18 19:10:29 +0300

    Done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,71 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(numberToWords(123));
+
+    }
+
+    public static boolean isPalindrome(int number) {
+        number = Math.abs(number);
+        char[] digits = String.valueOf(number).toCharArray();
+        String ters = "";
+        for (int i = digits.length - 1; i >= 0; i--) {
+            ters += digits[i];
+        }
+        return ters.equalsIgnoreCase(String.valueOf(number));
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        if (number < 0)
+            return false;
+        int total = 0;
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) {
+                total += i;
+            }
+        }
+        return number == total;
+    }
+    public static String numberToWords(int number){
+        if(number<0)return "Invalid Value";
+
+        char[] digits = String.valueOf(number).toCharArray();
+        String numToText = "";
+        for(char digit : digits){
+            switch (digit){
+                case '0':
+                  numToText +=  "Zero ";
+                  break;
+                case '1':
+                    numToText +=  "One ";
+                    break;
+                case '2':
+                    numToText +=  "Two ";
+                    break;
+                case '3':
+                    numToText +=  "Three ";
+                    break;
+                case '4':
+                    numToText +=  "Four ";
+                    break;
+                case '5':
+                    numToText +=  "Five ";
+                    break;
+                case '6':
+                    numToText +=  "Six ";
+                    break;
+                case '7':
+                    numToText +=  "Seven ";
+                    break;
+                case '8':
+                    numToText +=  "Eight ";
+                    break;
+                case '9':
+                    numToText +=  "Nine ";
+                    break;
+
+            }
+        }
+        return numToText.trim();
     }
 }
